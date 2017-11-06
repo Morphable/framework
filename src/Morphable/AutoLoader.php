e43b71e39404a717b05ef47bbd6754d1b0796162
Kobus <kobus.haarhuis@hotmail.com>
2017-11-06 10:51:22 +0100

    Autoload class

---
+++ src/Morphable/AutoLoader.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Morphable;
+
+class AutoLoader {
+
+  private $files = [];
+
+  function __constructor () {
+    spl_autoload_register([$this, '__autoload']);
+  }
+
+  public function addFolder ($folder) {
+    $files = \scandir($folder);
+    $files = array_slice($files, 2);
+    foreach ($files as $file) {
+      $this->files[] = $folder . '/' . $file;
+    }
+    return $this;
+  }
+
+  public function addFile ($file) {
+    $this->files[] = $file;
+    return $this;
+  }
+
+  private function __autoload ($class) {
+    if(file_exists($class)) {
+      require_once $class;
+    }
+  }
+
+  public function autoLoad () {
+    foreach ($this->files as $file) {
+      $this->__autoload($file);
+    }
+  }
+
+}
