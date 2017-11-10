ce4d31d322242dad5ec49318b916508c735ed494
Kobus <kobus.haarhuis@hotmail.com>
2017-11-10 15:04:52 +0100

    Rename autoload to lowercase

---
+++ autoload.php
@@ -0,0 +1,36 @@
+<?php
+
+$rootPath = __DIR__ . '/';
+$morphablePath = $rootPath . 'src/Morphable/';
+$databasePath = $morphablePath . 'Database/';
+$consolePath = $morphablePath . 'Console/';
+
+require $morphablePath . 'AutoLoader.php';
+
+$autoloader = new Morphable\Autoloader;
+
+/**
+ * Require Base
+ */
+$autoloader->addFile($morphablePath . 'Config.php');
+$autoloader->addFile($databasePath . 'Connection.php');
+$autoloader->addFile($databasePath . 'Manager.php');
+
+/**
+ * Require Migrations
+ */
+$migrationsPath = $databasePath . 'Migrations/';
+$autoloader->addFile($migrationsPath . 'Table.php');
+$autoloader->addFile($migrationsPath . 'Field.php');
+$autoloader->addFile($migrationsPath . 'TableBuilder.php');
+
+/**
+ * Require Console
+ */
+$autoloader->addFile($consolePath . 'Console.php');
+$autoloader->addFile($consolePath . 'Command.php');
+
+/**
+ * Execute
+ */
+$autoloader->autoload();
