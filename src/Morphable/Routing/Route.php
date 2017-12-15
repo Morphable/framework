aed3b0d1c21291607afec67851a2fb01a75b8a2a
Kobus <kobus.haarhuis@hotmail.com>
2017-12-15 09:08:51 +0100

    Remove old router

--- src/Morphable/Routing/Route.php
+++ src/Morphable/Routing/Route.php
@@ -2,46 +2,17 @@
 
 namespace Morphable\Routing;
 
-use Morphable\Http;
-
 class Route {
-
-  public $route;
-  public $middleware = [];
+  
   public $method;
-  public $callback;
-  public $prefix = '';
-  public $suffix = '';
-  public $req;
-  public $res;
+  public $route;
+  public $middleware;
 
-  function __construct ($method, $route, $middleware, $callback) {
+  function __construct ($method = 'get', $route, $middleware = []) {
     $this->method = $method;
     $this->route = $route;
     $this->middleware = $middleware;
 
-    $this->req = new Http\Request($this);
-    $this->res = new Http\Response($this);
-
-    if ($route == '404') {
-      $callback($this->req, $this->res);
-    } else {
-      if (RouteBuilder::compareMethod($_SERVER['REQUEST_METHOD'], $this->method)) {
-        RouteBuilder::buildAndCompare($_SERVER['REQUEST_URI'], $this->route, function ($params) use ($callback) {
-          $this->req->params = $params;
-          $this->execMiddleware($callback);
-          die;
-        });
-      }
-    }
-
-  }
-
-  private function execMiddleware ($callback) {
-    foreach ($this->middleware as $middleware) {
-      Router::runMiddleware($middleware, $this);
-    }
-    $callback($this->req, $this->res);
   }
 
   public function middleware ($middleware) {
@@ -52,6 +23,22 @@ class Route {
     }
 
     return $this;
+  }
+
+  public function get () {
+    return get_object_vars($this);
+  }
+
+  public function getMethod () {
+    return $this->method;
+  }
+
+  public function getRoute () {
+    return $this->route;
+  }
+
+  public function getMiddleware () {
+    return $this->middleware;
   }
 
 }
