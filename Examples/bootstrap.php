59a61083feea4122459236beb90f9867de5b46b6
kobus <dev.kobus@gmail.com>
2018-05-21 22:04:53 +0200

    test mysql migration builder again

--- Examples/bootstrap.php
+++ Examples/bootstrap.php
@@ -4,3 +4,9 @@ session_start();
 
 require __DIR__ . '/../autoload.php';
 require __DIR__ . '/../config.php';
+
+use \Morphable\Database\Connection;
+use \Morphable\Database\Manager;
+
+new Manager($appConfig['connection']);
+Connection::staticInstance($appConfig['connection']);
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
 require 'Examples/bootstrap.php';
+require 'Examples/migration.php';
 require 'Examples/router-v2.php';
-
-
